92207a231718d49ed47096bbbcc96afe3117d8cf
AnvarovD <doniyer.anvarov@alif.uz>
2024-11-25 16:40:35 +0500

    delete some files for nginx conf

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
-    return 'ok';
+    return view('welcome');
 });
 
 Route::get('/dashboard', function () {
